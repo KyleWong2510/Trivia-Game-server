126ad289cdbcfee4ee456cae1b8ff0e9c78f6a97
Kyle Wong <kylewong2510@gmail.com>
2020-07-26 13:42:38 -0600

    Set up boilerplate

--- server.js
+++ server.js
@@ -2,3 +2,19 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+app.set('port', process.env.PORT || 3001)
+app.locals.title = 'Trivia Game'
+
+app.use(cors())
+app.use(express.json())
+
+app.locals.scores = []
+
+app.get('/', (request, response) => {
+  response.send('Trivia Game API')
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+})
+
